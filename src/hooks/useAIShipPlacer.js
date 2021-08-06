4d591ccda6241813177b20abffe684bb2d1037b0
PaperOrb <48017994+PaperOrb@users.noreply.github.com>
2021-08-06 19:47:03 -0400

    add setTimeout to aiShipPlacer

--- src/hooks/useAIShipPlacer.js
+++ src/hooks/useAIShipPlacer.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 
 export default function useAIShipPlacer({ cpuBoard, setCurrentShip, aiPlacedShips, currentShip }) {
   useEffect(() => {
-    let ship;
     let shipsArr = ["carrier", "battleship", "patrolboat", "destroyer", "submarine"];
 
     shipsArr.forEach((ship, index) => {
@@ -18,18 +17,11 @@ export default function useAIShipPlacer({ cpuBoard, setCurrentShip, aiPlacedShip
         view: window,
       });
 
-      async function sleepDebugger() {
-        while (!aiPlacedShips.includes(ship)) {
-          function timer(ms) {
-            return new Promise((res) => setTimeout(res, ms));
-          }
-          let randomSq = document.getElementById(`cpu-square-${index + 1}0`);
-          shipEle.dispatchEvent(mouseDown);
-          randomSq.dispatchEvent(drop);
-          await timer(3000);
-        }
-      }
-      sleepDebugger();
+      setTimeout(() => {
+        let randomSq = document.getElementById(`cpu-square-${index + 1}0`);
+        shipEle.dispatchEvent(mouseDown);
+        randomSq.dispatchEvent(drop);
+      }, 20);
     });
   }, []);
 }
